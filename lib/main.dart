8024b2c76c93ee517306003b716ebe54cd988fe6
Andres Muñoz <elmelloflaco16@gmail.com>
2024-11-08 15:11:57 -0500

    Importé la librería foundation.dart para que la aplicación reconozca si está corriendo en web o en movil, para que así inicialize la ruta correspondiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+//----------------------------------------------------------------
+//----WEB!!!!!---------//
 import 'package:voces_de_aula/screens/login_screen.dart';
 import 'package:voces_de_aula/screens/main_screen.dart';
 import 'package:voces_de_aula/screens/profesor2.dart';
@@ -8,9 +11,11 @@ import 'package:voces_de_aula/screens/profile_screen.dart';
 import 'package:voces_de_aula/screens/register_screen.dart';
 import 'package:voces_de_aula/screens/home.dart';
 import 'package:voces_de_aula/screens/asignaturas.dart';
-
 import 'package:voces_de_aula/screens/visualizacionProfesores.dart';
 
+//----------MOVIL!!!!!-------------//
+
+//----------------------------------------------------------------
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -33,7 +38,9 @@ class MyApp extends StatelessWidget {
         '/profile': (_) => const ProfileScreen(),
         '/asignaturas': (_) => const Asignaturas(),
       },
-      initialRoute: '/asignaturas',
+      initialRoute: kIsWeb
+          ? '/main'
+          : '/asignaturas', // Usa rutas diferentes para web y móvil
     );
   }
 }
